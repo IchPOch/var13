cd3887ab6671807b1a30ecabe80aaa8dc7d349db
Михаил <@DESKTOP-3P2543V>
2022-05-26 16:54:39 +0300

    Добавьте файлы проекта.

---
+++ var13/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace var13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridVivod = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVivod)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 260);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 232);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Обзор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(127, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(179, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(130, 69);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ряд";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Столбец";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Строка";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 72);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Элементы ряда";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 22);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(77, 23);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridVivod);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 232);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridVivod
+            // 
+            this.dataGridVivod.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridVivod.Location = new System.Drawing.Point(20, 20);
+            this.dataGridVivod.Name = "dataGridVivod";
+            this.dataGridVivod.RowTemplate.Height = 25;
+            this.dataGridVivod.Size = new System.Drawing.Size(732, 190);
+            this.dataGridVivod.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 89);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Считать ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVivod)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridVivod;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ var13/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace var13
+{
+    public partial class Form1 : Form
+    {
+        int N = 4;
+        int M = 4;
+        List<double> chisla = new List<double>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Maximum = N;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(textBox1.Text))
+                {
+                    //double[,] A = new double[N, M];
+                    if(textBox1.Text == null)
+                    {
+                        throw new ArgumentNullException("");
+                    }
+                    string line;
+                    int c = 0;
+                    dataGridVivod.Rows.Clear();
+                    dataGridVivod.Columns.Clear();
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        string[] dat = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int j = 0; j < dat.Length; j++)
+                        {
+                            chisla.Add(double.Parse(dat[j]));
+                        }
+                    }
+                    //file.Close();
+                    //for(int i = 0; i < N; i++)
+                    //{
+                    //    for(int j = 0; j < M; j++)
+                    //    {
+                    //        A[i, j] = chisla[c];
+                    //        c++;
+                    //    }
+                    //}
+                    for (int i = 0; i < M; i++)
+                    {
+                        dataGridVivod.Columns.Add(i.ToString(), i.ToString());
+                    }
+                    for (int j = 1; j < N; j++)
+                    {
+                        dataGridVivod.Rows.Add("0");
+                    }
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < M; j++)
+                        {
+                            dataGridVivod.Rows[i].Cells[j].Value = chisla[c];
+                            c++;
+                        }
+                    }
+                }
+                using (StreamWriter fileVvod = new StreamWriter(textBox1.Text,true))
+                {
+                    fileVvod.WriteLine();
+                    if (radioButton1.Checked)
+                    {
+                        double[] B = new double[M];
+                        for (int i = 0; i < M; i++)
+                        {
+                            B[i] = Convert.ToDouble(dataGridVivod.Rows[Convert.ToInt32(numericUpDown1.Value) - 1].Cells[i].Value);
+                            dataGridVivod.Rows[Convert.ToInt32(numericUpDown1.Value) - 1].Cells[i].Style.BackColor = System.Drawing.Color.Red;
+                        }
+                        for (int i = 0; i < B.Length; i++)
+                        {
+                            fileVvod.Write(string.Format("{0} ", B[i]));
+                        }
+                    }
+                    else
+                    {
+                        double[] B = new double[N];
+                        for (int i = 0; i < N; i++)
+                        {
+                            B[i] = Convert.ToDouble(dataGridVivod.Rows[i].Cells[Convert.ToInt32(numericUpDown1.Value) - 1].Value);
+                            dataGridVivod.Rows[i].Cells[Convert.ToInt32(numericUpDown1.Value) - 1].Style.BackColor = System.Drawing.Color.Red;
+                        }
+                        for (int i = 0; i < B.Length; i++)
+                        {
+                            fileVvod.Write(string.Format(" {0} ", B[i]));
+                        }
+                    }
+                }
+
+            }
+            catch(ArgumentNullException)
+            {
+                MessageBox.Show("Выберите путь к файлу");
+            }
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = N;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = M;
+        }
+    }
+}
